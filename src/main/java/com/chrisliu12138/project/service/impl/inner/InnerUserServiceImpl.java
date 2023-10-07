6e144746c77ff4c8bfe6c1eaa8007978d1eca881
chrisliu12138 <liuhao.hi@qq.com>
2023-10-07 21:46:22 +0800

    add sub-projects

--- src/main/java/com/chrisliu12138/project/service/impl/inner/InnerUserServiceImpl.java
+++ src/main/java/com/chrisliu12138/project/service/impl/inner/InnerUserServiceImpl.java
@@ -1,30 +1,30 @@
-package com.chrisliu12138.project.service.impl.inner;
-
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.chrisliu12138.chrisapicommon.model.entity.User;
-import com.chrisliu12138.chrisapicommon.service.InnerUserService;
-import com.chrisliu12138.project.common.ErrorCode;
-import com.chrisliu12138.project.exception.BusinessException;
-import com.chrisliu12138.project.mapper.UserMapper;
-import org.apache.commons.lang3.StringUtils;
-//import org.apache.dubbo.config.annotation.DubboService;
-
-import javax.annotation.Resource;
-
-//@DubboService
-public class InnerUserServiceImpl implements InnerUserService {
-
-    @Resource
-    private UserMapper userMapper;
-
-
-    @Override
-    public User getInvokeUser(String accessKey) {
-        if (StringUtils.isAnyBlank(accessKey)) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR);
-        }
-        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("accessKey", accessKey);
-        return userMapper.selectOne(queryWrapper);
-    }
-}
+//package com.chrisliu12138.project.service.impl.inner;
+//
+//import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+//import com.chrisliu12138.chrisapicommon.model.entity.User;
+//import com.chrisliu12138.chrisapicommon.service.InnerUserService;
+//import com.chrisliu12138.project.common.ErrorCode;
+//import com.chrisliu12138.project.exception.BusinessException;
+//import com.chrisliu12138.project.mapper.UserMapper;
+//import org.apache.commons.lang3.StringUtils;
+////import org.apache.dubbo.config.annotation.DubboService;
+//
+//import javax.annotation.Resource;
+//
+////@DubboService
+//public class InnerUserServiceImpl implements InnerUserService {
+//
+//    @Resource
+//    private UserMapper userMapper;
+//
+//
+//    @Override
+//    public User getInvokeUser(String accessKey) {
+//        if (StringUtils.isAnyBlank(accessKey)) {
+//            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+//        }
+//        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+//        queryWrapper.eq("accessKey", accessKey);
+//        return userMapper.selectOne(queryWrapper);
+//    }
+//}
