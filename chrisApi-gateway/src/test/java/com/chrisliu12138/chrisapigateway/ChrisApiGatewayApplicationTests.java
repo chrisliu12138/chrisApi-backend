896cb200e1bdecc10cf69bc2ebf26939d2b60337
chrisliu12138 <liuhao.hi@qq.com>
2023-10-10 17:15:28 +0800

    update gateway

---
+++ chrisApi-gateway/src/test/java/com/chrisliu12138/chrisapigateway/ChrisApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisliu12138.chrisapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrisApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
